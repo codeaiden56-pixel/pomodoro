63245e803a8cf75d14127c7aa0827b92e78f62d5
codeaiden56-pixel <codeaiden56@gmail.com>
2025-11-15 11:09:54 -0800

    commit2

--- src/App.js
+++ src/App.js
@@ -6,28 +6,19 @@ function App() {
   const [running, setRunning] = useState(false);
   const [timeRemaining, setTimeRemaining] = useState(1500);
   const [currentCycleIndex, setCurrentCycleIndex] = useState(0);
+  const [timeCycleStarted, setTimeCycleStarted] = useState(null);
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
 
   const cycle = [
-    {
-      name: "Pomodoro",
-      time: 1500,
-    },
-    {
-      name: "5-min Break",
-      time: 300,
-    },
-    {
-      name: "5-min Break",
-      time: 300,
-    },
-    {
-      name: "5-min Break",
-      time: 300,
-    },
-    {
-      name: "15-min Break",
-      time: 900,
-    },
+    { name: "Focus", time: 1500 },
+    { name: "Short Break", time: 300 },
+    { name: "Focus", time: 1500 },
+    { name: "Short Break", time: 300 },
+    { name: "Focus", time: 1500 },
+    { name: "Short Break", time: 300 },
+    { name: "Focus", time: 1500 },
+    { name: "Long Break", time: 1800 },
   ];
 
   function convertSecondsToTime(totalSeconds) {
@@ -40,23 +31,29 @@ function App() {
     return `${formattedMinutes}:${formattedSeconds}`;
   }
 
+  function calculateTimeDifference(currentTime) {
+    return (currentTime.getTime() - timeCycleStarted.getTime()) / 1000;
+  }
+
   function setNextStageInCycle() {
     let utterance = new SpeechSynthesisUtterance(
-      cycle[(currentCycleIndex + 1) % cycle.length].name
+      "Time for " + cycle[(currentCycleIndex + 1) % cycle.length].name
     );
     speechSynthesis.speak(utterance);
     setCurrentCycleIndex((currentCycleIndex + 1) % cycle.length);
     setTimeRemaining(cycle[(currentCycleIndex + 1) % cycle.length].time);
+    setTimeCycleStarted(new Date());
   }
 
   useEffect(() => {
     if (running) {
-      const timeoutId = setTimeout(
-        () => setTimeRemaining(timeRemaining - 1),
-        1000
-      );
+      const timeoutId = setTimeout(() => {
+        setTimeRemaining(
+          cycle[currentCycleIndex].time - calculateTimeDifference(new Date())
+        );
+      }, 100);
       if (timeRemaining <= 0) {
-        setCurrentCycleIndex();
+        setNextStageInCycle();
       }
       return () => clearTimeout(timeoutId);
     }
@@ -64,30 +61,98 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <h3>{cycle[currentCycleIndex].name}</h3>
-        <h1>{convertSecondsToTime(timeRemaining)}</h1>
+      <div className="Pomodoro">
+        <h3 className="Stage">{cycle[currentCycleIndex].name}</h3>
+        <h1 className="Timer">
+          {convertSecondsToTime(Math.floor(timeRemaining))}
+        </h1>
         <p>Next: {cycle[(currentCycleIndex + 1) % cycle.length].name}</p>
-        <button onClick={() => setRunning(!running)}>
-          {running ? "Pause" : "Start"}
-        </button>
-        <button
-          onClick={() => {
-            setRunning(false);
-            setCurrentCycleIndex(0);
-            setTimeRemaining(cycle[0].time);
-          }}
-        >
-          Reset
-        </button>
-        <button
-          onClick={() => {
-            setNextStageInCycle();
-          }}
-        >
-          Skip
-        </button>
+        <div className="Button-bar">
+          <button
+            onClick={() => {
+              setRunning(!running);
+              if (timeCycleStarted === null) {
+                setTimeCycleStarted(new Date());
+              }
+            }}
+          >
+            {running ? "Pause" : "Start"}
+          </button>
+          {!running ? (
+            <button
+              onClick={() => {
+                setRunning(false);
+                setCurrentCycleIndex(0);
+                setTimeRemaining(cycle[0].time);
+              }}
+            >
+              Reset
+            </button>
+          ) : null}
+          {running ? (
+            <button
+              onClick={() => {
+                setNextStageInCycle();
+              }}
+            >
+              Skip
+            </button>
+          ) : null}
+        </div>
       </div>
+
+      <div className="Task-container">
+        <div className="Task-container-header-container">
+          <h2 className="Task-container-header">
+            Focus Tasks ({tasks.length})
+          </h2>
+        </div>
+        <div className="Task-container-input-bar">
+          <input
+            placeholder="Create Task"
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+          />
+          <button
+            onClick={() => {
+              if (newTask !== "") {
+                setTasks([...tasks, newTask]);
+                setNewTask("");
+              }
+            }}
+          >
+            Add
+          </button>
+        </div>
+        <div className="Task-container-main">
+          {tasks.length > 0 ? (
+            tasks.map((task, index) => <TaskCard key={index} task={task} />)
+          ) : (
+            <p style={{ color: "gray", textAlign: "left" }}>No tasks</p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+function TaskCard({ task }) {
+  const [complete, setComplete] = useState(false);
+  return (
+    <div className="Taskcard">
+      <div
+        className={complete ? "Checkbox-complete" : "Checkbox"}
+        onClick={() => {
+          setComplete(!complete);
+        }}
+      />
+      <p
+        style={{
+          textDecorationLine: complete ? "line-through" : "",
+          color: complete ? "gray" : "black",
+        }}
+      >
+        {task}
+      </p>
     </div>
   );
 }
